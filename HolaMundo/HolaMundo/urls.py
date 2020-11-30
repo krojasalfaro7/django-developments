c3e620f0685b9877779eb5587ef207cc5b49e870
Kevin Rojas <krojas.alfaro7@gmail.com>
2020-11-30 18:30:55 -0400

    quepasa

--- HolaMundo/HolaMundo/urls.py
+++ HolaMundo/HolaMundo/urls.py
@@ -17,7 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from HolaMundo.views import saludo
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
-]
+    path('/', saludo),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
